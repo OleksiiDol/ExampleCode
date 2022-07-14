2d1b00114fa4f06c39ee4785f1d78df53cd56247
OleksiiDol <106844702+OleksiiDol@users.noreply.github.com>
2022-07-14 15:25:20 +0300

    Add exapmle code

---
+++ ActorSpawner.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Server.Core.Accounts;
+using Server.Core.Data.DtoWrappers;
+using Server.Core.Spawns;
+using Server.Core.Storage.Utility;
+using Server.Core.StorageActors.LootActor;
+using Server.Core.Utility.Logging;
+using Server.Core.Worlds;
+using Vector3 = Server.Core.Networking.Generated.Vector3;
+
+namespace Server.Core.Storage.Loot
+{
+    public class ServerLootActorSpawner
+    {
+        private readonly object _locker = new();
+        private readonly InstanceServer _instanceServer;
+        private readonly List<LootActor> _lootStorageActors = new();
+        private readonly IdentifiersPool _lootActorsIds = new();
+        private const int LOOTING_RADIUS = 1000;
+
+        public ServerLootActorSpawner(InstanceServer instanceServer)
+        {
+            _instanceServer = instanceServer;
+        }
+
+        public void OnNpcDead(Spawn spawnNpc)
+        {
+            if (spawnNpc.Prototype.Dto.GlobalLootTableNames.Count <= 0 
+                && spawnNpc.Prototype.Dto.UniqueLootTableNames.Count <= 0) return;
+
+            var aggressors = new List<Character>();
+            var battlePawns = spawnNpc.StateFlowController.AggroRegister.Aggressors;
+            
+            foreach (var battlePawn in battlePawns)
+            {
+                if (battlePawn is not Character aggressor) continue;
+                aggressors.Add(aggressor);
+            }
+
+            CreateActor(aggressors, spawnNpc.Position, spawnNpc.Prototype);
+        }
+
+        private void CreateActor(List<Character> aggressors, Vector3 position, NpcWrapper npcWrapper)
+        {
+            lock (_locker)
+            {
+                if (aggressors == null || aggressors.Count == 0)
+                {
+                    Debug.Error($"Not found aggressors after death {npcWrapper?.Name}", Debug.Pane.Loot);
+                    return;
+                }
+               
+                var globalLootTables = Model.Instance.Data.LootTablesProvider.GetTables(npcWrapper.Dto.GlobalLootTableNames);
+                var uniqueLootTables = Model.Instance.Data.LootTablesProvider.GetTables(npcWrapper.Dto.UniqueLootTableNames);
+                
+                var loot = new Dictionary<int, List<Item>>();
+                
+                foreach (var table in globalLootTables)
+                {
+                    var lootItems = _instanceServer.LootGenerator.GenerateLoot(table);
+
+                    if (lootItems.Count > 0)
+                    {
+                        if (loot.TryGetValue(CONSTANTS.GENERAL_LOOT_OWNER_ID, out var items))
+                        {
+                            items.AddRange(lootItems);
+                        }
+                        else
+                        {
+                            loot.Add(CONSTANTS.GENERAL_LOOT_OWNER_ID, lootItems);
+                        }
+                    }
+                }
+                
+                foreach (var aggressor in aggressors)
+                {
+                    foreach (var table in uniqueLootTables)
+                    {
+                        var lootItems = _instanceServer.LootGenerator.GenerateLoot(table);
+                    
+                        if (lootItems.Count > 0)
+                        {
+                            if (loot.TryGetValue(aggressor.SeenActorId, out var items))
+                            {
+                                items.AddRange(lootItems);
+                            }
+                            else
+                            {
+                                loot.Add(aggressor.SeenActorId, lootItems);
+                            }
+                        }
+                    }
+                }
+
+                if (loot.Count == 0) return;
+
+                var actor = new LootActor(_lootActorsIds.Get(), position, npcWrapper.Dto.LootConfigShortCode, DespawnActor);
+                
+                actor.CreateStorage(loot);
+
+                var currentAggressors = !loot.ContainsKey(CONSTANTS.GENERAL_LOOT_OWNER_ID)
+                    ? aggressors.FindAll(a => loot.ContainsKey(a.SeenActorId))
+                    : aggressors;
+
+                actor.SetAggressors(currentAggressors);
+
+                _lootStorageActors.Add(actor);
+                
+                SpawnLootForAggressors(currentAggressors, actor);
+            }
+        }
+
+        private void SpawnLootForAggressors(List<Character> characters, SeenActor actor)
+        {
+            lock (_locker)
+            {
+                foreach (var aggressor in characters)
+                {
+                    aggressor.Send(actor.SpawnPacket);
+                }
+            }
+        }
+
+        private void DespawnActor(LootActor actor)
+        {
+            lock (_locker)
+            {
+                foreach (var aggressor in actor.GetAggressors().ToList())
+                {
+                    aggressor.Send(actor.DespawnPacket);
+                    actor.RemoveAggressor(aggressor);
+                }
+                
+                _lootStorageActors.Remove(actor);
+                _lootActorsIds.Release(actor.SeenActorId);
+            }
+        }
+        
+        public void SendSpawnActors(Character pawn)
+        {
+            lock (_locker)
+            {
+                foreach (var actor in _lootStorageActors)
+                {
+                    if (actor.ContainsAggressor(pawn.SeenActorId))
+                    {
+                        pawn.Send(actor.SpawnPacket);
+                    }
+                }
+            }
+        }
+
+        public List<LootActor> GetActorsInRadius(Character owner)
+        {
+            lock (_locker)
+            {
+                return _lootStorageActors.Where(actor =>
+                    actor.ContainsAggressor(owner.SeenActorId) &&
+                    actor.Position.Distance(owner.Position) <= LOOTING_RADIUS).ToList();
+            }
+        }
+    }
+}
---
+++ LootActor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Protobuf;
+using Server.Core.Accounts;
+using Server.Core.Location;
+using Server.Core.Networking.Generated;
+using Server.Core.Spawns;
+using Server.Core.Storage.Inventories;
+using Server.Core.Storage.Utility;
+using Server.Core.Utility;
+using Vector3 = Server.Core.Networking.Generated.Vector3;
+
+namespace Server.Core.StorageActors.LootActor
+{
+    public class LootActor : SeenActor
+    {
+        public event Action<LootActor> OnActorDespawn;
+        public LootActorInventory Storage { get; private set; }
+        
+        private readonly Action<LootActor> _removeCallback;
+        private List<Character> _allAggressors;
+        private CallbackWrapper _despawnTimer;
+        private const int DESPAWN_TIMER_VALUE = 60000;
+        private readonly string _shortCode;
+      
+        public override IMessage SpawnPacket
+        {
+            get
+            {
+                var message = new PckSpawnLootActor
+                {
+                    LootId = SeenActorId,
+                    Position = Position,
+                    ShortCode = _shortCode
+                };
+
+                return message;
+            }
+        }
+
+        public override IMessage DespawnPacket
+        {
+            get
+            {
+                var message = new PckDespawnLootActor
+                {
+                    ActorId = SeenActorId
+                };
+                
+                return message;
+            }
+        }
+        
+        public LootActor(IdentifiersPool.Identifier id, Vector3 position, string confShortCode, Action<LootActor> removeCallback)
+        {
+            SeenActorId = id;
+            Position = position;
+            SeenList = new PawnSeenList(this);
+
+            _shortCode = confShortCode;
+            _removeCallback = removeCallback;
+        }
+        
+        public void CreateStorage(Dictionary<int, List<Item>> loot)
+        {
+            Storage = new LootActorInventory(this, loot);
+
+            _despawnTimer = Model.Instance.Heart.CallOnce(RemoveActor, DESPAWN_TIMER_VALUE, false);
+        }
+
+        public void SetAggressors(List<Character> aggressors)
+        {
+            _allAggressors = aggressors;
+        }
+
+        public List<Character> GetAggressors()
+        {
+            return _allAggressors;
+        }
+        
+        public bool ContainsAggressor(int seenId)
+        {
+            return _allAggressors.Any(actor => actor.SeenActorId == seenId);
+        }
+
+        public void RemoveAggressor(Character aggressor)
+        {
+            _allAggressors.Remove(aggressor);
+        }
+
+        public void RemoveActor()
+        {
+            _despawnTimer?.Cancel();
+
+            OnActorDespawn?.Invoke(this);
+
+            _removeCallback?.Invoke(this);
+        }
+    }
+}
